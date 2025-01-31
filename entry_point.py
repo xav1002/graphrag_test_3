033dc113fb983f1ea8efb8e747523d64ea0d9cc3
Vincent <xuvincent1002@gmail.com>
2025-01-30 22:36:20 -0600

    docker complete

--- entry_point.py
+++ entry_point.py
@@ -2,3 +2,9 @@ import subprocess
 
 # start ollama server
 subprocess.run("ollama serve",shell=True)
+
+subprocess.run('python -m graphrag prompt-tune --root . --config ./settings.yaml --domain "salt-tolerant microbial species"',shell=True)
+
+subprocess.run('python -m graphrag index --root .',shell=True)
+
+subprocess.run('python -m graphrag query --root . --method global --query "Please list salt-tolerant microbial species."',shell=True)
